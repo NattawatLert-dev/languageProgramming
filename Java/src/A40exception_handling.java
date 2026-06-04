8588a6ed4ab42c1b0a2d0b566aa28a0dac30f28c
nattawat lertchidumrongsri <nattawatlert0747@gmail.com>
2026-06-04 18:42:14 +0700

    Exceptions

---
+++ Java/src/A40exception_handling.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class A40exception_handling {
+    public static void main(String[] args) {
+        
+        // Exception = An event that interrupts the normal flow of a program
+        //             (Dividing by zero, file not found, mismatch input type)
+        //             Surround any dangerous code with a try{} block
+        //             try{}, catch{}, finally{}
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        try{
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+            System.out.println(number);
+        }
+        catch(Exception e){
+            // SAFETY NET
+            System.out.println("Something went wrong");
+        }
+        finally{
+            System.out.println("This always executes");
+        }
+    }
+}
