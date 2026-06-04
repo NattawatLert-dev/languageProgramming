4e8859dd1a747e071fe339e9b1dc275825505b31
nattawat lertchidumrongsri <nattawatlert0747@gmail.com>
2026-06-04 11:25:09 +0700

    SETTER and GETTER

---
+++ Java/src/a14gettersandsetters/Car.java
@@ -0,0 +1,43 @@
+package a14gettersandsetters;
+
+public class Car {
+    
+    private final String model;
+    private String color;
+    private int price;
+
+    public Car(String model, String color, int price) {
+        this.model = model;
+        this.color = color;
+        this.price = price;
+    }
+    
+    String getModel(){
+        return this.model;
+    }
+
+    String getColor(){
+        return this.color;
+    }
+
+    int getPrice(){
+        return this.price;
+    }
+
+    /*void setModel(String model){
+        this.model = model;
+    } */
+
+    void setColor(String color){
+        this.color = color;
+    }
+
+    void setPrice(int price){
+        if(price < 0){
+            System.out.println("Price can't be less than zero");
+        }else{
+            this.price = price;
+        }
+    }
+    
+}
---
+++ Java/src/a14gettersandsetters/Test.java
@@ -0,0 +1,18 @@
+package a14gettersandsetters;
+
+public class Test {
+    public static void main(String[] args) {
+        
+        // They help protect object data and add rules for accessing or modifying them.
+        // GETTERS = Methods that make a field READABLE.
+        // SETTERS = Methods that make a field WRITEABLE.
+    
+        Car car = new Car("Charger", "Yellow", 10000);
+
+        car.setColor("Red");
+        car.setPrice(13422);
+
+        System.out.println(car.getColor() + " " + car.getModel() + " " + car.getPrice());
+    
+    }
+}
