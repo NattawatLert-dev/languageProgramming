fdfdf26258b914224978566f2fc5f12f43c81047
nattawat lertchidumrongsri <nattawatlert0747@gmail.com>
2026-06-04 20:04:04 +0700

    Project Music player

---
+++ Java/src/A43musicplayer.java
@@ -0,0 +1,63 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class A43musicplayer {
+    public static void main(String[] args) {
+        
+        // How to PLAY AUDIO with Java (.wav, .au, .aiff)
+
+        String filePath = "src\\A Caring Friend.wav";
+        File file = new File(filePath);
+
+        try(Scanner scanner = new Scanner(System.in);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file)) {
+
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+
+            String response = "";
+
+            while(!response.equals("Q")){
+                System.out.println("P = Play");
+                System.out.println("S = Stop");
+                System.out.println("R = Reset");
+                System.out.println("Q = Quit");
+                System.out.println("Enter your choice: ");
+                response = scanner.next().toUpperCase();
+
+                switch (response) {
+                    case "P" -> clip.start();
+                    case "S" -> clip.stop();
+                    case "R" -> clip.setMicrosecondPosition(0);
+                    case "Q" -> clip.close();
+                    default -> System.out.println("Invalid choice");
+                }
+            }
+
+            System.out.println("NO problems detected");
+        } 
+        catch (FileNotFoundException e){
+            System.out.println("Could not locate file");
+        }
+        catch (UnsupportedAudioFileException e) {
+            System.out.println("Audio file is not supported");
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong");
+        } 
+        catch (LineUnavailableException e){
+            System.out.println("Unable to access audio resource");
+        }
+        finally {
+            System.out.println("Bye!");
+        }
+    }
+}
