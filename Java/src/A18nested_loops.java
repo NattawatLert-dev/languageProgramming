2bb888e727f785dbbba5008872f36051a4c40f4c
nattawat lertchidumrongsri <nattawatlert0747@gmail.com>
2026-06-05 15:15:36 +0700

    Nested Loops

--- Java/src/A18nested_loops.java
+++ Java/src/A18nested_loops.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class A25nested_loops {
+public class A18nested_loops {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
