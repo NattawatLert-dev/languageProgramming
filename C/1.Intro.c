76e8affea19f882a590aa5e3680fcb3983335b41
nattawat lertchidumrongsri <nattawatlert0747@gmail.com>
2026-06-05 08:18:39 +0700

    Introduction in C

---
+++ C/1.Intro.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+
+    // This is my first program in C
+
+    /*
+    This
+    is 
+    also
+    a
+    comment
+    */
+
+    printf("I like Pizza!\n");
+    printf("It's really good!");
+   
+    return 0;
+}
